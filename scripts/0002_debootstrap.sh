5344d4b4a08d96ab840704440cf226e919779ff0
Christopher Obbard <obbardc@gmail.com>
2017-11-10 21:34:34 +0000

    fix uboot compile instructions

--- scripts/0002_debootstrap.sh
+++ scripts/0002_debootstrap.sh
@@ -2,11 +2,11 @@
 
 # debootstrap a basic Debian system
 info "Debootstrapping system (first-stage)"
-debootstrap --arch=arm64 --foreign buster $ROOTFS
+#debootstrap --arch=arm64 --foreign buster $ROOTFS
 
 # copy in the ARM static binary (so we can chroot)
 cp /usr/bin/qemu-aarch64-static $ROOTFS/usr/bin/
 
 # actually install the packages
 info "Debootstrapping system (second-stage)"
-chroot_exec /debootstrap/debootstrap --second-stage
+#chroot_exec /debootstrap/debootstrap --second-stage
