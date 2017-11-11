5a75ec7e1fdb3071bf8f78ef83373bab458a7855
Christopher Obbard <obbardc@gmail.com>
2017-11-11 19:51:42 +0000

    fix u-boot parameters

--- scripts/0003_copy_homegrown_kernel.sh
+++ scripts/0003_copy_homegrown_kernel.sh
@@ -25,14 +25,15 @@ echo "All your sunxi are belong to us."
 echo "---------------"
 echo "booting..."
 
+setenv load_addr "0x44000000"
 setenv bootargs "console=ttyS0,115200 console=tty0 disp.screen0_output_mode=1280x720p60 root=/dev/mmcblk0p1 panic=10"
 
-load mmc 0:1 0x11000000 boot/Image
-load mmc 0:1 0x1000000 boot/sun50i-h5-orangepi-pc2.dtb
-fdt addr 0x1000000
+load mmc 0:1 ${load_addr} boot/Image
+load mmc 0:1 ${fdt_addr_r} boot/sun50i-h5-orangepi-pc2.dtb
+fdt addr ${fdt_addr_r}
 
 
-booti 0x11000000 - 0x1000000
+booti ${load_addr} - ${fdt_addr_r}
 EOF
 
 $KERNEL_TMP/u-boot/tools/mkimage -C none -A arm64 -T script -d $ROOTFS/boot/boot.cmd $ROOTFS/boot.scr
