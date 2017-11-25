0d4e8904f17a5f73662e1918b58fb6ba7f1ab986
Christopher Obbard <obbardc@gmail.com>
2017-11-25 15:28:51 +0000

    enable ethernet in userspace

--- scripts/1000_system_setup.sh
+++ scripts/1000_system_setup.sh
@@ -11,9 +11,19 @@ chroot_exec usermod -p "${ENCRYPTED_PASSWORD}" root
 # mount root filesystem
 echo "/dev/mmcblk0p1	/	ext4	defaults,noatime	0	1" > $ROOTFS/etc/fstab
 
+# setup wired network (dhcp)
+echo "
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+allow-hotplug eth0
+iface eth0 inet dhcp" >> $ROOTFS/etc/interfaces
+
 # setup apt
-#echo "deb http://ftp.uk.debian.org/debian buster main
-#deb-src http://ftp.uk.debian.org/debian buster main" > $ROOTFS/etc/apt/sources.list
+echo "deb http://ftp.uk.debian.org/debian buster main
+deb-src http://ftp.uk.debian.org/debian buster main" > $ROOTFS/etc/apt/sources.list
 
 # update repo
 #chroot_exec apt-get update
